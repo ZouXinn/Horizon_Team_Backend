9049464e2a3d729a7b185f3a6b30c8f154d7a6f0
thoucsin <1598244350@qq.com>
2019-11-13 00:58:36 +0800

    aaa

--- ProgramAST.cpp
+++ ProgramAST.cpp
@@ -34,16 +34,42 @@ Value* ProgramAST::codegen() {
 
 	updateBB();
 
-
+	
 	for (int i = this->dec_exp_ASTs->size() - 1; i >= 0; i--)
 	{
 		this->dec_exp_ASTs->at(i)->codegen();
+		//GlobalVariable()
+		//Builder.CreateG
 	}
 
 
 	for (int i = this->func_define_ASTs->size() - 1; i >= 0; i--)
 	{
 		this->func_define_ASTs->at(i)->codegen();
+		NamedValues.clear();
+		Params.clear();
+		//NamedValues = std::map<std::string, AllocaInst*>();
 	}
 	return nullptr;
+}
+
+void ProgramAST::RunJIT()
+{
+	// JIT the module containing the anonymous expression, keeping a handle so
+	  // we can free it later.
+	auto H = TheJIT->addModule(std::move(TheModule));
+	InitializeModuleAndPassManager();
+
+	// Search the JIT for the __anon_expr symbol.
+	auto ExprSymbol = TheJIT->findSymbol("main");
+	assert(ExprSymbol && "未找到main函数");//到时候应该要修改
+
+	// Get the symbol's address and cast it to the right type (takes no
+	// arguments, returns a double) so we can call it as a native function.
+
+    int (*FP)() = (int (*)())(intptr_t)cantFail(ExprSymbol.getAddress());
+	fprintf(stderr, "main exited with code %d\n", FP());
+
+	// Delete the anonymous expression module from the JIT.
+	TheJIT->removeModule(H);
 }
